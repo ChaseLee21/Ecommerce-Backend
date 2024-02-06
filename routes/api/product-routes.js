1aada0499e5a16824a2006ce568b61c2583d385d
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-06 13:55:20 -0800

    created delete method in product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -133,6 +133,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(() => {
+    res.status(200).json({ message: 'Product deleted' });
+  })
 });
 
 module.exports = router;
